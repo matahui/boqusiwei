9405e74be5ed3da7e5493fbfde84689e6a661926
lihui6 <lihui6@xunlei.cn>
2024-08-29 14:46:45 +0800

    update

--- config/database.go
+++ config/database.go
@@ -1,12 +1,14 @@
 package config
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	sl "homeschooledu/logger"
 	"os"
+	"time"
 )
 
 var DB *gorm.DB
@@ -18,7 +20,7 @@ func InitDB() {
 		return
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=30s",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
@@ -31,15 +33,49 @@ func InitDB() {
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger:gormLogger,
 	})
+
 	if err != nil {
 		//panic("failed to connect to database")
 		sl.Log.Fatal("Database connected failed", err)
 		return
 	}
 
+	sqlDB, err := DB.DB()
+	if err != nil {
+		sl.Log.Fatal("Database getDB failed", err)
+		return
+	}
+
+	// 设置最大空闲连接数
+	sqlDB.SetMaxIdleConns(10)
+
+	// 设置最大打开连接数
+	sqlDB.SetMaxOpenConns(100)
+
+	// 设置连接的最大可复用时间
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
 	sl.Log.Info("Database connected successfully.")
+
+	go func() {
+		ticker := time.NewTicker(10 * time.Second)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			printDBStats(sqlDB)
+		}
+	}()
 }
 
 func GetDB() *gorm.DB {
 	return DB
+}
+
+func printDBStats(db *sql.DB) {
+	stats := db.Stats()
+	sl.Log.Infof("连接池状态 - 空闲连接数: %d, 打开连接数: %d, 最大打开连接数: %d, 等待中的连接数: %d\n",
+		stats.Idle,
+		stats.OpenConnections,
+		stats.MaxOpenConnections,
+		stats.WaitCount)
 }
